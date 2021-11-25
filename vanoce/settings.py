d7eae21974a89f64932516966cc70f0f276b9316
basileia <jane.novak@centrum.cz>
2021-11-25 21:44:30 +0100

    update db settings-try

--- vanoce/settings.py
+++ vanoce/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = "vanoce.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
-        'PORT': config('DATABASE_PORT', default='5432'),
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPASS'),
+        'HOST': config('DBHOST'),
+        'PORT': config('DBPORT', default='5432'),
     }
 }
 
