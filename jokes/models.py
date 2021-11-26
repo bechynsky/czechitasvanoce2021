6010e5ea47264bbd70f35b7867392a88bd06a321
basileia <jane.novak@centrum.cz>
2021-11-26 16:49:00 +0100

    new model Joke

--- jokes/models.py
+++ jokes/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField("Název", max_length=30, unique=True)
+
+
+class Joke(models.Model):
+    name = models.TextField("Název")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, verbose_name="Kategorie")
