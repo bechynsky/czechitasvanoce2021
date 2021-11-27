51001b679a0b501537204bed55a977e88f898462
basileia <jane.novak@centrum.cz>
2021-11-27 09:25:15 +0100

    first jokes to db

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Joke
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the jokes index.")
+    joke = Joke.objects.get(id=1)
+    # return render(request, "jokes/randomJoke.html", {"joke": joke})
+    return HttpResponse(joke.content)
